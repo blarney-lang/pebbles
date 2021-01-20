6c233604960e1c93942e2a3d62578cc31d237500
Matthew Naylor <mn416@cam.ac.uk>
2021-01-20 20:18:05 +0000

    New pipeline stage in coalescing unit
    (Preparing to generalise coalescing unit)

--- src/Pebbles/Memory/CoalescingUnit.hs
+++ src/Pebbles/Memory/CoalescingUnit.hs
@@ -60,14 +60,15 @@ data CoalescingInfo t_id =
 -- lane id, are satisfied by a single DRAM burst; (2) SameAddress:
 -- mutliple accesses to the same address are satisifed by a single
 -- DRAM access. The second strategy (which always makes progress)
--- is used if the first fails.  We employ a six stage pipeline:
+-- is used if the first fails.  We employ a seven stage pipeline:
 --
 --   0. Consume requests and feed pipeline
 --   1. Pick one SIMT lane as a leader 
 --   2. Determine the leader's request with a mux
---   3. Choose coalescing strategy and feed back unsatisifed reqs to stage 1
---   4. Issue DRAM requests
---   5. Consume DRAM responses and issue load responses
+--   3. Evaluate coalescing strategies
+--   4. Choose coalescing strategy and feed back unsatisifed reqs to stage 1
+--   5. Issue DRAM requests
+--   6. Consume DRAM responses and issue load responses
 --
 -- Notes:
 --   * The number of SIMT lanes is assumed to be equal to the
@@ -94,26 +95,39 @@ makeCoalescingUnit dramResps = do
   go1 :: Reg (Bit 1) <- makeDReg false
   go2 :: Reg (Bit 1) <- makeDReg false
   go3 :: Reg (Bit 1) <- makeDReg false
-  go4 :: Reg (Bit 1) <- makeReg false
+  go4 :: Reg (Bit 1) <- makeDReg false
+  go5 :: Reg (Bit 1) <- makeReg false
 
   -- Requests for each pipeline stage
   memReqs1 :: [Reg (MemReq t_id)] <- replicateM SIMTLanes (makeReg dontCare)
   memReqs2 :: [Reg (MemReq t_id)] <- replicateM SIMTLanes (makeReg dontCare)
   memReqs3 :: [Reg (MemReq t_id)] <- replicateM SIMTLanes (makeReg dontCare)
   memReqs4 :: [Reg (MemReq t_id)] <- replicateM SIMTLanes (makeReg dontCare)
+  memReqs5 :: [Reg (MemReq t_id)] <- replicateM SIMTLanes (makeReg dontCare)
 
   -- Pending request mask for each pipeline stage
   pending1 :: Reg (Bit SIMTLanes) <- makeReg dontCare
   pending2 :: Reg (Bit SIMTLanes) <- makeReg dontCare
   pending3 :: Reg (Bit SIMTLanes) <- makeReg dontCare
+  pending4 :: Reg (Bit SIMTLanes) <- makeReg dontCare
 
   -- Leader requests for each pipeline stage
   leaderReq3 :: Reg (MemReq t_id) <- makeReg dontCare
   leaderReq4 :: Reg (MemReq t_id) <- makeReg dontCare
+  leaderReq5 :: Reg (MemReq t_id) <- makeReg dontCare
 
   -- Bit vector identifying the chosen leader
   leader2 :: Reg (Bit SIMTLanes) <- makeReg 0
   leader3 :: Reg (Bit SIMTLanes) <- makeReg 0
+  leader4 :: Reg (Bit SIMTLanes) <- makeReg 0
+
+  -- Choice of strategy
+  coalSameBlockStrategy4 :: Reg (Bit 1) <- makeReg dontCare
+  coalSameBlockStrategy5 :: Reg (Bit 1) <- makeReg dontCare
+
+  -- Which lanes are participating in the strategy
+  coalMask4 :: Reg (Bit SIMTLanes) <- makeReg dontCare
+  coalMask5 :: Reg (Bit SIMTLanes) <- makeReg dontCare
 
   -- DRAM request queue
   dramReqQueue :: Queue (DRAMReq DRAMReqId) <- makePipelineQueue 1
@@ -159,6 +173,7 @@ makeCoalescingUnit dramResps = do
       go1 <== go1.val
       go2 <== go2.val
       go3 <== go3.val
+      go4 <== go4.val
 
   -- Stage 1: Pick a leader
   -- ======================
@@ -189,14 +204,8 @@ makeCoalescingUnit dramResps = do
       pending3 <== pending2.val
       leader3 <== leader2.val
 
-  -- Stage 3: Select coalescing strategy
-  -- ===================================
-
-  -- Choice of strategy
-  coalSameBlockStrategy :: Reg (Bit 1) <- makeReg dontCare
-
-  -- Which lanes are participating in the strategy
-  coalMask :: Reg (Bit SIMTLanes) <- makeReg dontCare
+  -- Stage 3: Evaluate coalescing strategies
+  -- =======================================
 
   always do
     -- Which requests can be satisfied by SameBlock strategy?
@@ -249,11 +258,6 @@ makeCoalescingUnit dramResps = do
               .&. (r.memReqAccessWidth .==. leaderReq3.val.memReqAccessWidth)
           | (p, r) <- zip (pending3.val.toBitList) (map val memReqs3) ]
 
-    -- This strategy should at least allow the leader to make progress
-    when (go3.val) do
-      dynamicAssert (sameAddrMask .!=. 0)
-        "Coalescing Unit: SameAddr strategy does not make progress!"
-
     -- Feed back unsatisfied requests
     -- ------------------------------
 
@@ -268,36 +272,53 @@ makeCoalescingUnit dramResps = do
     -- Requests participating in strategy
     let mask = useSameBlock ? (sameBlockMask, sameAddrMask)
 
+    when (go3.val .&. stallWire.val.inv) do
+      -- SameAddr should at least allow the leader to make progress
+      dynamicAssert (sameAddrMask .!=. 0)
+        "Coalescing Unit: SameAddr strategy does not make progress!"
+      -- Trigger next stage
+      go4 <== true
+      coalSameBlockStrategy4 <== useSameBlock
+      coalMask4 <== mask
+      zipWithM_ (<==) memReqs4 (map val memReqs3)
+      pending4 <== pending3.val
+      leaderReq4 <== leaderReq3.val
+      leader4 <== leader3.val
+
+  -- Stage 4: Choose coalescing strategy
+  -- ===================================
+
+  always do
     -- Try to trigger next stage
-    when (go3.val) do
+    when (go4.val) do
       -- Check if stage 4 is currently busy
-      if go4.val
+      if go5.val
         then do
           -- If so, stall pipeline
           stallWire <== true
         else do
-          -- Otherwise, setup and trigger stage 4
-          go4 <== true
-          leaderReq4 <==
-            (leaderReq3.val) {
+          -- Otherwise, setup and trigger next stage
+          go5 <== true
+          leaderReq5 <==
+            (leaderReq4.val) {
               memReqData =
                 -- Align data field of leader request
-                writeAlign (leaderReq3.val.memReqAccessWidth)
-                           (leaderReq3.val.memReqData)
+                writeAlign (leaderReq4.val.memReqAccessWidth)
+                           (leaderReq4.val.memReqData)
             }
-          zipWithM_ (<==) memReqs4 (map val memReqs3)
-          coalSameBlockStrategy <== useSameBlock
-          coalMask <== mask
+          zipWithM_ (<==) memReqs5 (map val memReqs4)
+          coalSameBlockStrategy5 <== coalSameBlockStrategy4.val
+          coalMask5 <== coalMask4.val
           -- Determine any remaining pending requests
-          let remaining = pending3.val .&. inv mask
+          let remaining = pending4.val .&. inv (coalMask4.val)
           -- If there are any, feed them back
           when (remaining .!=. 0) do
             go1 <== true
             feedbackWire <== true
-            zipWithM_ (<==) memReqs1 (map val memReqs3)
+            zipWithM_ (<==) memReqs1 (map val memReqs4)
             pending1 <== remaining
 
-  -- Stage 4: Issue DRAM requests
+  -- Stage 5: Issue DRAM requests
   -- ============================
 
   -- Count register for burst store
@@ -305,10 +326,10 @@ makeCoalescingUnit dramResps = do
 
   always do
     -- Shorthands for chosen strategy
-    let useSameBlock = coalSameBlockStrategy.val
-    let mask = coalMask.val
+    let useSameBlock = coalSameBlockStrategy5.val
+    let mask = coalMask5.val
     -- Shorthand for access width
-    let aw = leaderReq4.val.memReqAccessWidth
+    let aw = leaderReq5.val.memReqAccessWidth
     -- Determine burst length and address mask (to align the burst)
     let (burstLen, addrMask) :: (DRAMBurst, Bit 1) =
           if useSameBlock
@@ -320,26 +341,26 @@ makeCoalescingUnit dramResps = do
               ]
             else (1, 0b0)
     -- The DRAM address is derived from the top bits of the memory address
-    let dramAddr = slice @31 @DRAMBeatLogBytes (leaderReq4.val.memReqAddr)
+    let dramAddr = slice @31 @DRAMBeatLogBytes (leaderReq5.val.memReqAddr)
     -- DRAM data field for SameBlock strategy
     let sameBlockData8 :: V.Vec DRAMBeatBytes (Bit 8) =
           V.fromList $ concat $ replicate 2
-            [r.val.memReqData.truncate | r <- memReqs4]
+            [r.val.memReqData.truncate | r <- memReqs5]
     let sameBlockData16 :: V.Vec DRAMBeatHalfs (Bit 16) =
-          V.fromList [r.val.memReqData.truncate | r <- memReqs4]
+          V.fromList [r.val.memReqData.truncate | r <- memReqs5]
     let sameBlockData32 :: V.Vec DRAMBeatWords (Bit 32) =
           V.fromList $ selectHalf (storeCount.val.truncate)
-            [r.val.memReqData | r <- memReqs4]
+            [r.val.memReqData | r <- memReqs5]
     let sameBlockData :: DRAMBeat =
           [pack sameBlockData8,
              pack sameBlockData16,
                pack sameBlockData32] ! aw
     -- DRAM data field for SameAddress strategy
     let sameAddrDataVec :: V.Vec DRAMBeatWords (Bit 32) =
-          V.replicate (leaderReq4.val.memReqData)
+          V.replicate (leaderReq5.val.memReqData)
     let sameAddrData :: DRAMBeat = pack sameAddrDataVec
     -- DRAM byte enable field for SameBlock strategy
-    let useUpper = at @(DRAMBeatLogBytes-1) (leaderReq4.val.memReqAddr)
+    let useUpper = at @(DRAMBeatLogBytes-1) (leaderReq5.val.memReqAddr)
     let sameBlockBE8 :: Bit DRAMBeatBytes =
           fromBitList $
             [en .&. aw.isByteAccess .&. useUpper | en <- mask.toBitList] ++
@@ -353,9 +374,9 @@ makeCoalescingUnit dramResps = do
               [en .&. aw.isWordAccess | en <- mask.toBitList]
     let sameBlockBE = orList [sameBlockBE8, sameBlockBE16, sameBlockBE32]
     -- DRAM byte enable field for SameAddress strategy
-    let leaderBE = genByteEnable (leaderReq4.val.memReqAccessWidth)
-                    (leaderReq4.val.memReqAddr)
-    let subWord = slice @DRAMBeatLogBytes @2 (leaderReq4.val.memReqAddr)
+    let leaderBE = genByteEnable (leaderReq5.val.memReqAccessWidth)
+                    (leaderReq5.val.memReqAddr)
+    let subWord = slice @DRAMBeatLogBytes @2 (leaderReq5.val.memReqAddr)
     let sameAddrBEVec :: V.Vec DRAMBeatWords (Bit 4) = 
           V.fromList [ subWord .==. fromInteger i ? (leaderBE, 0)
                      | i <- [0..DRAMBeatWords-1] ]
@@ -364,14 +385,14 @@ makeCoalescingUnit dramResps = do
     let dramReq =
           DRAMReq {
             dramReqId = ()
-          , dramReqIsStore = leaderReq4.val.memReqOp .==. memStoreOp
+          , dramReqIsStore = leaderReq5.val.memReqOp .==. memStoreOp
           , dramReqAddr = dramAddr.truncate .&. addrMask.zeroExtend.inv
           , dramReqData = useSameBlock ? (sameBlockData, sameAddrData)
           , dramReqByteEn = useSameBlock ? (sameBlockBE, sameAddrBE)
           , dramReqBurst = burstLen
           }
     -- Try to issue DRAM request
-    when (go4.val) do
+    when (go5.val) do
       -- Check that we can make a DRAM request
       when (inflightQueue.notFull .&. dramReqQueue.notFull) do
         -- Issue DRAM request
@@ -381,27 +402,27 @@ makeCoalescingUnit dramResps = do
               CoalescingInfo {
                 coalInfoUseSameBlock = useSameBlock
               , coalInfoMask = mask
-              , coalInfoReqIds = V.fromList [r.val.memReqId | r <- memReqs4]
-              , coalInfoIsUnsigned = leaderReq4.val.memReqIsUnsigned
-              , coalInfoAccessWidth = leaderReq4.val.memReqAccessWidth
-              , coalInfoAddr = leaderReq4.val.memReqAddr.truncate
+              , coalInfoReqIds = V.fromList [r.val.memReqId | r <- memReqs5]
+              , coalInfoIsUnsigned = leaderReq5.val.memReqIsUnsigned
+              , coalInfoAccessWidth = leaderReq5.val.memReqAccessWidth
+              , coalInfoAddr = leaderReq5.val.memReqAddr.truncate
               , coalInfoBurstLen = burstLen - 1
               }
         -- Handle load: insert info into inflight queue
-        when (leaderReq4.val.memReqOp .==. memLoadOp) do
+        when (leaderReq5.val.memReqOp .==. memLoadOp) do
           enq inflightQueue info
-          go4 <== false
+          go5 <== false
         -- Handle store: increment burst count
-        when (leaderReq4.val.memReqOp .==. memStoreOp) do
+        when (leaderReq5.val.memReqOp .==. memStoreOp) do
           let newStoreCount = storeCount.val + 1
           if newStoreCount .==. burstLen
             then do
               storeCount <== newStoreCount
             else do
               storeCount <== 0
-              go4 <== false
+              go5 <== false
 
-  -- Stage 5: Handle DRAM responses
+  -- Stage 6: Handle DRAM responses
   -- ==============================
 
   -- Count register for burst load
