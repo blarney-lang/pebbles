036cd450ae134fed28cb08183eef01a42c874b64
Matthew Naylor <mn416@cam.ac.uk>
2021-02-23 11:36:02 +0000

    DRAM: dont interleave requests during write burst

--- src/Pebbles/SoC/DRAM/DualPort.hs
+++ src/Pebbles/SoC/DRAM/DualPort.hs
@@ -40,12 +40,59 @@ makeDRAMDualPort (reqs0, reqs1) avlIns = do
   let dramReqs0 = fmap (tag 0) reqs0
   let dramReqs1 = fmap (tag 1) reqs1
 
-  -- Fair merger for requests
-  dramReqs <- makeGenericFairMergeTwo makeQueue (const 1)
-                (dramReqs0, dramReqs1)
+  -- Merge buffer
+  buffer <- makeQueue
+
+  -- Input streams
+  let inA = dramReqs0
+  let inB = dramReqs1
+
+  -- Locks on input streams (to avoid interleaving write bursts)
+  lockA :: Reg (Bit 1) <- makeReg false
+  lockB :: Reg (Bit 1) <- makeReg false
+
+  -- Burst counts for each stream
+  burstA :: Reg DRAMBurst <- makeReg 1
+  burstB :: Reg DRAMBurst <- makeReg 1
+
+  -- Was previous output taken from stream A?
+  prevChoiceWasA :: Reg (Bit 1) <- makeReg false
+
+  always do
+    when (buffer.notFull) do
+      -- Take next input from stream B?
+      let chooseB = lockB.val .|.
+            (lockA.val ? (false,
+              inB.canPeek .&. (inA.canPeek.inv .|. prevChoiceWasA.val)))
+      -- Consume input
+      if chooseB
+        then when (inB.canPeek) do
+          inB.consume
+          when (inB.peek.dramReqIsStore) do
+            if burstB.val .==. inB.peek.dramReqBurst
+              then do
+                lockB <== false
+                burstB <== 1
+              else do
+                lockB <== true
+                burstB <== burstB.val + 1
+        else when (inA.canPeek) do
+          inA.consume
+          when (inA.peek.dramReqIsStore) do
+            if burstA.val .==. inA.peek.dramReqBurst
+              then do
+                lockA <== false
+                burstA <== 1
+              else do
+                lockA <== true
+                burstA <== burstA.val + 1
+      -- Produce output
+      when (inA.canPeek .|. inB.canPeek) do
+        enq buffer (chooseB ? (inB.peek, inA.peek))
+        prevChoiceWasA <== inv chooseB
 
   -- Single DRAM instance
-  (dramResps, avlOuts) <- makeDRAM dramReqs avlIns
+  (dramResps, avlOuts) <- makeDRAM (buffer.toStream) avlIns
 
   -- Get the tag from the response
   let getTag resp = resp.dramRespId.fst
