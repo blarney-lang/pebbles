704254e00f7caa60f7550b0af8005dbc40e0fc99
Matthew Naylor <mn416@cam.ac.uk>
2021-04-19 11:49:28 +0100

    Test script

--- apps/TestSuite/TestCPU.cpp
+++ apps/TestSuite/TestCPU.cpp
@@ -6,20 +6,25 @@ int main(int argc, char** argv)
 {
   HostLink hostLink;
 
+  bool failed = false;
   for (int i = 1; i < argc; i++) {
     char codeFile[1024];
     char dataFile[1024];
     int n = strlen(argv[i]);
     if (argv[i][n-1] == 'S' && argv[i][n-2] == '.') argv[i][n-2] = '\0';
-    printf("%-16s", argv[i]); fflush(stdout);
+    printf("  %-16s", argv[i]); fflush(stdout);
     snprintf(codeFile, sizeof(codeFile), "%s.cpu.code.v", argv[i]);
     snprintf(dataFile, sizeof(dataFile), "%s.cpu.data.v", argv[i]);
     hostLink.boot(codeFile, dataFile);
     hostLink.startCPU();
     uint8_t result = hostLink.uart->getByte();
-    if (result == 1) printf("pass\n");
-    else printf("FAIL(%d)\n", result >> 1);
+    if (result == 1)
+      printf("ok\n");
+    else {
+      printf("FAILED(%d)\n", result >> 1);
+      failed = true;
+    }
   }
 
-  return 0;
+  return failed ? -1 : 0;
 }
